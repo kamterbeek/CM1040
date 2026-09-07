4a9cd39fcb2c2167dd187a2670cd998034cd143e
Kassandra <kam.terbeek@gmail.com>
2026-09-06 21:19:06 -0600

    Add SimpleTemplateEngine class
    Implement SimpleTemplateEngine class to load templates from a URL.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+class SimpleTemplateEngine {
+constructor(template_url) {
+this.template_url = template_url;
+}
 
+  loadTemplate(){
+  return fetch(this.template_url)
+    .then(response => response.text())
+    .then(text => {
+      
+    }
+  }
+}
