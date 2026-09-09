9b8597694b7bd1acfe60516f5d0098dcd63aae5b
Kassandra <kam.terbeek@gmail.com>
2026-09-08 20:11:04 -0600

    Refactor SimpleTemplateEngine methods and logic

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 class SimpleTemplateEngine {
-constructor(template_url) {
-this.template_url = template_url;
-}
-
-  loadTemplate(){
-  return fetch(this.template_url)
-    .then(response => response.text())
-    .then(text => {
-      this.template = text;
-    });
-  }
-renderTemplate(tag_id, data) {
-  const tag = document.getElementById(tag_id);
-  let output = this.template;
-  
-// each loops
-  output = output.replace(/{{#each (\w)}}([\s\S]*?){{\/each}}/g, match, arrayName, templateFragment) => {
-    const listOfThings = data[arrayName];
-
-    if (!Array.isArray(listOfThings)) {
-      return '';
+    constructor(template_url) {
+        this.template_url = template_url;
     }
-    return listOfThings.map(item => this.replaceVariablesInFragment(templateFragment, item)).join('');
-    
-  });
 
-// If-Else conditions
+    loadTemplate() {
+        return fetch(this.template_url)
+            .then(response => response.text())
+            .then(text => {
+                this.template = text;
+            });
+    }
+
+    renderTemplate(tag_id, data) {
+        const tag = document.getElementById(tag_id);
+        let output = this.template;
+
+        // Each loops
+        output = output.replace(/{{#each (\w+)}}([\s\S]*?){{\/each}}/g, (match, arrayName, templateFragment) => {
+            const listOfThings = data[arrayName];
+
+            if (!Array.isArray(listOfThings)) {
+                return '';
+            }
+            return listOfThings.map(item => this.replaceVariablesInFragment(templateFragment, item)).join('');
+        });
+
+        // If-Else conditions
         output = output.replace(/{{#if (\w+)}}([\s\S]*?){{else}}([\s\S]*?){{\/if}}/g, (match, condition, ifContent, elseContent) => {
             return data[condition] ? ifContent : elseContent;
         });
@@ -34,41 +34,45 @@ renderTemplate(tag_id, data) {
         output = output.replace(/{{#if (\w+)}}([\s\S]*?){{\/if}}/g, (match, condition, ifContent) => {
             return data[condition] ? ifContent : '';
         });
-  
-//variable swapping
-  output = output.replace(/{{\w+)}}/g, (match, datafield) => {
-    return data[dataField];
-  });
-  
-  tag.innerHTML = output;              
-}
-  replaceVariablesInFragment(templateFragment, data) {
-    return templateFragment.replace(/{{(\w+)}}/g, (match, dataKey) => {
-      return data[dataKey];
-    });
-  }
+
+        // Variable swapping
+        output = output.replace(/{{(\w+)}}/g, (match, dataField) => {
+            return data[dataField];
+        });
+
+        tag.innerHTML = output;
+    }
+
+    replaceVariablesInFragment(templateFragment, data) {
+        return templateFragment.replace(/{{(\w+)}}/g, (match, dataKey) => {
+            return data[dataKey];
+        });
+    }
 }
 
-// create an instance and load the template
+// Create an instance and load the template
 const tEngine = new SimpleTemplateEngine('template.html');
 
-const data = {
-  title: "Page title",
-  content: "Page content",
-  subtitle: "Page sub title",
-  loggedIn: true,
-  username: "nobody",
-  subcontent: "Page sub content",
-  items: [
-    { title: "item 1", description: "Desc 1" },
-    { title: "item 2", description: "Desc 2"}
-  ]
-  
+let data = {
+    loaded: false
 };
 
-
 tEngine.loadTemplate().then(() => {
-  console.log('Template loaded:', tEngine.template);
-  tEngine.renderTemplate("content", data);
-});
+    console.log('Template loaded:', tEngine.template);
 
+    // Fetch data from the REST API at intervals
+    setInterval(() => {
+        console.log("Reloading data");
+        fetch("http://localhost:3000/books")
+            .then(response => response.json())
+            .then(jsonData => {
+                console.log(jsonData);
+                data = {
+                    title: "Book list",
+                    loaded: true,
+                    books: jsonData
+                };
+                tEngine.renderTemplate('content', data);
+            });
+    }, 4000);
+});
